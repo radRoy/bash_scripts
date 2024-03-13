4ffa8eb205a9eabbe25f975a616a507aacf12a0d
radRoy <daniel.walther@uzh.ch>
2024-03-13 13:24:14 +0100

    Update 02-sbatch-predict3dunet-3dunet1.8.2.sh

--- slurm/02-sbatch-predict3dunet-3dunet1.8.2.sh
+++ slurm/02-sbatch-predict3dunet-3dunet1.8.2.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:A100:1
-#SBATCH --time=30:00
+#SBATCH --time=1:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=daniel.walther@uzh.ch
 
